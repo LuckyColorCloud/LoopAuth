95ebf32f817a8bcabe3176787a870eecc4cfe0a6
Sober <sober1050@qq.com>
2022-09-07 10:29:16 +0800

    本月

--- LoopAuth-plugin/LoopAuth-abac/src/main/java/com/sobercoding/loopauth/abac/model/authProperty/IntervalType.java
+++ LoopAuth-plugin/LoopAuth-abac/src/main/java/com/sobercoding/loopauth/abac/model/authProperty/IntervalType.java
@@ -2,6 +2,8 @@ package com.sobercoding.loopauth.abac.model.authProperty;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * 时间区间属性类型
@@ -41,6 +43,18 @@ public enum IntervalType {
     MONTH{
         @Override
         public long creation(){
+            Calendar calendar = Calendar.getInstance();//获取当前日期
+            calendar.add(Calendar.MONTH, 0);//n代表和本月偏移 0本月、1后一月，-1前一月
+            calendar.set(Calendar.DAY_OF_MONTH, 1);//设置为1号,当前日期既为本月第一天
+            calendar.set(Calendar.HOUR_OF_DAY,0);//将小时至0
+            calendar.set(Calendar.MINUTE,0);//将分钟至0
+            calendar.set(Calendar.SECOND,0);//将秒至0
+            long start = calendar.getTime().getTime();
+            //下方为月末的最后一天
+            calendar.add(Calendar.MONTH, 1);
+            calendar.set(Calendar.SECOND,-1);
+            long end = calendar.getTime().getTime();
+
             return 0;
         }
     },
