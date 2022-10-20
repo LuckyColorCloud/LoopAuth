8d148d429c67cb3a76a504f2d59c4bdfd2672570
Sober <sober1050@qq.com>
2022-10-21 00:10:15 +0800

    开始适配webflux

---
+++ springwebflux-test/src/main/java/com/sobercoding/loopauth/SpringwebfluxTestApplication.java
@@ -0,0 +1,13 @@
+package com.sobercoding.loopauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebfluxTestApplication.class, args);
+    }
+
+}
